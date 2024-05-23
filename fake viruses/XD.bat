54b77a90ece07d6e65117b9f5269231d33652d09
KaaayKaay <137423230+KaaayKaay@users.noreply.github.com>
2024-05-23 17:28:34 +0100

    Add files via upload

---
+++ fake viruses/XD.bat
@@ -0,0 +1,370 @@
+@echo OFF
+title XD / DESTROYING PC
+color 0a
+
+:1
+
+cls
+echo \
+echo / The XD Virus Malware initiative is initiating a full override and destruction of your computer.
+echo \ [#---------]
+echo /
+echo \ Taking discord ID
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \           ,__
+echo /  [\    /] [  \
+echo \  \ \  / / [   \
+echo /   \ \/ /  [ [\ \
+echo \   / /\ \  [ [/ /
+echo /  / /  \ \ [   /
+echo \  [/    \] [__/
+timeout /t 1 /nobreak >nul
+
+cls
+echo \
+echo / The XD Virus Malware initiative is initiating a full override and destruction of your computer..
+echo \ [##--------]
+echo /
+echo \ Getting your IP
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \           ,__
+echo /  [\    /] [  \
+echo \  \ \  / / [   \
+echo /   \ \/ /  [ [\ \
+echo \   / /\ \  [ [/ /
+echo /  / /  \ \ [   /
+echo \  [/    \] [__/
+timeout /t 1 /nobreak >nul
+
+cls
+echo \
+echo / The XD Virus Malware initiative is initiating a full override and destruction of your computer...
+echo \ [###-------]
+echo /
+echo \ IP found and uploaded!
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \           ,__
+echo /  [\    /] [  \
+echo \  \ \  / / [   \
+echo /   \ \/ /  [ [\ \
+echo \   / /\ \  [ [/ /
+echo /  / /  \ \ [   /
+echo \  [/    \] [__/
+timeout /t 1 /nobreak >nul
+
+cls
+echo \
+echo / The XD Virus Malware initiative is initiating a full override and destruction of your computer.
+echo \ [####------]
+echo /
+echo \ Attempting to locate your bank details
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \           ,__
+echo /  [\    /] [  \
+echo \  \ \  / / [   \
+echo /   \ \/ /  [ [\ \
+echo \   / /\ \  [ [/ /
+echo /  / /  \ \ [   /
+echo \  [/    \] [__/
+timeout /t 1 /nobreak >nul
+
+cls
+echo \
+echo / The XD Virus Malware initiative is initiating a full override and destruction of your computer..
+echo \ [#####-----]
+echo /
+echo \ Bank details found and uploaded!
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \           ,__
+echo /  [\    /] [  \
+echo \  \ \  / / [   \
+echo /   \ \/ /  [ [\ \
+echo \   / /\ \  [ [/ /
+echo /  / /  \ \ [   /
+echo \  [/    \] [__/
+timeout /t 1 /nobreak >nul
+
+cls
+echo \
+echo / The XD Virus Malware initiative is initiating a full override and destruction of your computer...
+echo \ [######----]
+echo /
+echo \ Alerting authorities of your location
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \           ,__
+echo /  [\    /] [  \
+echo \  \ \  / / [   \
+echo /   \ \/ /  [ [\ \
+echo \   / /\ \  [ [/ /
+echo /  / /  \ \ [   /
+echo \  [/    \] [__/
+timeout /t 1 /nobreak >nul
+
+cls
+echo \
+echo / The XD Virus Malware initiative is initiating a full override and destruction of your computer.
+echo \ [#######---]
+echo /
+echo \ Calling authorities.
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \           ,__
+echo /  [\    /] [  \
+echo \  \ \  / / [   \
+echo /   \ \/ /  [ [\ \
+echo \   / /\ \  [ [/ /
+echo /  / /  \ \ [   /
+echo \  [/    \] [__/
+timeout /t 1 /nobreak >nul
+cls
+
+cls
+echo \
+echo / The XD Virus Malware initiative is initiating a full override and destruction of your computer..
+echo \ [########--]
+echo /
+echo \ Calling authorities..
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \           ,__
+echo /  [\    /] [  \
+echo \  \ \  / / [   \
+echo /   \ \/ /  [ [\ \
+echo \   / /\ \  [ [/ /
+echo /  / /  \ \ [   /
+echo \  [/    \] [__/
+timeout /t 1 /nobreak >nul
+cls
+
+cls
+echo \
+echo / The XD Virus Malware initiative is initiating a full override and destruction of your computer...
+echo \ [#########-]
+echo /
+echo \ Calling authorities...
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \           ,__
+echo /  [\    /] [  \
+echo \  \ \  / / [   \
+echo /   \ \/ /  [ [\ \
+echo \   / /\ \  [ [/ /
+echo /  / /  \ \ [   /
+echo \  [/    \] [__/
+timeout /t 1 /nobreak >nul
+cls
+
+echo \
+echo / The XD Virus Malware initiative is initiating a full override and destruction of your computer.
+echo \ [##########]
+echo /
+echo \ [CALL ANSWERED] - Results pending / [DATA UPLOADED] - uploaded 489 total passwords to webuyanydata.co.ck
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \
+echo /
+echo \           ,__
+echo /  [\    /] [  \
+echo \  \ \  / / [   \
+echo /   \ \/ /  [ [\ \
+echo \   / /\ \  [ [/ /
+echo /  / /  \ \ [   /
+echo \  [/    \] [__/
+timeout /t 4 /nobreak >nul
+
+echo \           ,__
+echo /  [\    /] [  \
+echo \  \ \  / / [   \
+echo /   \ \/ /  [ [\ \
+echo \   / /\ \  [ [/ /
+echo /  / /  \ \ [   /
+echo \  [/    \] [__/
+echo \           ,__
+echo /  [\    /] [  \
+echo \  \ \  / / [   \
+echo /   \ \/ /  [ [\ \
+echo \   / /\ \  [ [/ /
+echo /  / /  \ \ [   /
+echo \  [/    \] [__/
+echo \           ,__
+echo /  [\    /] [  \
+echo \  \ \  / / [   \
+echo /   \ \/ /  [ [\ \
+echo \   / /\ \  [ [/ /
+echo /  / /  \ \ [   /
+echo \  [/    \] [__/
+echo \           ,__
+echo /  [\    /] [  \
+echo \  \ \  / / [   \
+echo /   \ \/ /  [ [\ \
+echo \   / /\ \  [ [/ /
+echo /  / /  \ \ [   /
+echo \  [/    \] [__/
+echo /
+
+cd %~dp0
+start .\XD.bat
+
+cd %~dp0
+start .\XD.bat
+
+cd %~dp0
+start .\XD.bat
+
+timeout /t 1 /nobreak >nul
+
+mshta javascript:alert("Your PC will be destroyed shortly.\nThe authorities have been alerted and we have told them you are holding hostages at gunpoint.");close();
